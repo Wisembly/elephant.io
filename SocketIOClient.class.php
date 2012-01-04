3e5e3735b7a49086f69fa65e35385d3612ae4a3c
Barreca Ludovic <ludogp2@gmail.com>
2012-01-04 21:05:42 +0100

    adding connect method

--- SocketIOClient.class.php
+++ SocketIOClient.class.php
@@ -7,21 +7,34 @@
  * @author Ludovic Barreca <ludovic@balloonup.com>
  */
 class SocketIOClient {
-    private $serverUrl;
+    private $socketIOUrl;
+    private $serverHost;
+    private $serverPort = 80;
     private $session;
+    private $fd;
 
-    public function __construct($serverUrl) {
-        $this->serverUrl = $serverUrl;
+    public function __construct($socketIOUrl) {
+        $this->socketIOUrl = $socketIOUrl;
+        $this->parseUrl();
+    }
+
+    /**
+     * Initialize a new connection
+     *
+     */
+    public function init() {
         $this->handshake();
+        $this->connect();
     }
 
     /**
      * Handshake with socket.io server
      *
+     * @access public
      * @return bool
      */
     public function handshake() {
-        $ch = curl_init($this->serverUrl);
+        $ch = curl_init($this->socketIOUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $res = curl_exec($ch);
 
@@ -34,9 +47,64 @@ class SocketIOClient {
         $this->session['heartbeat_timeout'] = $sess[1];
         $this->session['connection_timeout'] = $sess[2];
         $this->session['supported_transports'] = array_flip(explode(',', $sess[3]));
-
-        if (!isset($session['supported_transports']['websocket'])) {
+        if (!isset($this->session['supported_transports']['websocket'])) {
             throw new \Exception('This socket.io server do not support websocket protocol. Terminating connection...');
+        }
+
+        return true;
+    }
+
+    /**
+     * Connects using websocket protocol
+     *
+     * @access public
+     * @return bool
+     */
+    public function connect() {
+        $this->fd = fsockopen($this->serverHost, $this->serverPort, $errno, $errstr);
+
+        if (!$this->fd) {
+            throw new \Exception('fsockopen returned: '.$errstr);
+        }
+
+        $out  = "GET /socket.io/1/websocket/".$this->session['sid']." HTTP/1.1\r\n";
+        $out .= "Upgrade: WebSocket\r\n";
+        $out .= "Connection: Upgrade\r\n";
+        $out .= "Host: ".$this->serverHost."\r\n";
+        $out .= "Origin: *\r\n\r\n";
+        fwrite($this->fd, $out);
+        $res = fgets($this->fd);
+
+        if ($res === false) {
+            throw new \Exception('socket.io did not respond properly. Aborting...');
+        }
+
+        if ($subres = substr($res, 0, 12) != 'HTTP/1.1 101') {
+            throw new \Exception('Unexpected Response. Expected HTTP/1.1 101 got '.$subres.'. Aborting...');
+        }
+
+        while(true) {
+            $res = trim(fgets($this->fd));
+            if ($res === '') break;
+        }
+    }
+
+    /**
+     * Parse the url and set server parameters
+     *
+     * @access private
+     * @return bool
+     */
+    private function parseUrl() {
+        $url = parse_url($this->socketIOUrl);
+        $this->serverHost = $url['host'];
+        $this->serverPort = isset($url['port']) ? $url['port'] : null;
+
+        if ($url['scheme'] == 'https') {
+            $this->serverHost = 'ssl://'.$this->serverHost;
+            if (!$this->serverPort) {
+                $this->serverPort = 443;
+            }
         }
 
         return true;
