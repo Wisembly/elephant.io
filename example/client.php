85bcba4b26f9f6f51a790f5277d06cf3d7614234
Barreca Ludovic <ludogp2@gmail.com>
2012-02-13 16:05:15 +0100

    updating example

--- example/client.php
+++ example/client.php
@@ -2,8 +2,9 @@
 
 <?php
 
-require('../ElephantIOClient.class.php');
+require('ElephantIOClient.class.php');
 
 $elephant = new ElephantIOClient('http://localhost:1337');
 
-$elephant->init();
+$elephant->init(false);
+$elephant->send(ElephantIOClient::TYPE_MESSAGE, null, null, 'Hello World!');
