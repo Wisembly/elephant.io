a75739ffec2d8f3523be13d37f57c815044e6092
Guillaume Potier <cobalt2760@gmail.com>
2013-04-22 16:47:13 +0200

    - fixed some undefined vars + fix Client->generateKey accessibility scope

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -2,12 +2,15 @@
 
 require_once(__DIR__.'/../lib/ElephantIO/Client.php');
 
+use ElephantIO\Client;
+
 class ClientTest extends PHPUnit_Framework_TestCase
 {
     public function testGenerateKey() {
-        $client = new ElephantIO\Client('http://localhost.net');
+        $reflectionMethod = new ReflectionMethod('ElephantIO\Client', 'generateKey');
+        $reflectionMethod->setAccessible(true);
 
-        $key = $client->generateKey();
+        $key = $reflectionMethod->invoke(new Client('http://localhost.net'));
 
         $this->assertEquals(24, strlen($key));
     }
