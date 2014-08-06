0519a0a5241be04b18c35ca8a9a43e8611f01b13
Baptiste Clavié <clavie.b@gmail.com>
2014-08-06 18:41:35 +0200

    Change Session's property transports into upgrades

--- src/Engine/SocketIO/Session.php
+++ src/Engine/SocketIO/Session.php
@@ -27,22 +27,24 @@ class Session
     /** @var integer session's timeout */
     private $timeout;
 
-    /** @var string[] supported transports */
-    private $transports;
+    /** @var string[] supported upgrades */
+    private $upgrades;
 
-    public function __construct($id, $heartbeat, $timeout, array $transports)
+    public function __construct($id, $heartbeat, $timeout, array $upgrades)
     {
-        $this->id         = $id;
-        $this->timeout    = $timeout;
-        $this->heartbeat  = $heartbeat;
-        $this->transports = $transports;
+        $this->id        = $id;
+        $this->timeout   = $timeout;
+        $this->upgrades  = $upgrades;
+        $this->heartbeat = $heartbeat;
     }
 
     /** The property should not be modified, hence the private accessibility on them */
     public function __get($prop)
     {
-        if (!in_array($prop, ['id', 'heartbeat', 'timeout', 'transports'])) {
-            throw new InvalidArgumentException(sprintf('Unknown property "%s" for the Session object', $prop));
+        static $list = ['id', 'heartbeat', 'timeout', 'upgrades'];
+
+        if (!in_array($prop, $list)) {
+            throw new InvalidArgumentException(sprintf('Unknown property "%s" for the Session object. Only the following are availables : ["%s"]', $prop, implode('", "', $list)));
         }
 
         return $this->$prop;
