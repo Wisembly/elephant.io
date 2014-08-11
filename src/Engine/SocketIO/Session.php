f10b4254a0fe8e7f2c6ff1b8b6489a6a06cc8792
Baptiste Clavié <clavie.b@gmail.com>
2014-08-11 17:08:43 +0200

    Refactor heartbeat handling in the Session

--- src/Engine/SocketIO/Session.php
+++ src/Engine/SocketIO/Session.php
@@ -26,30 +26,48 @@ class Session
     /** @var integer session's last heartbeat */
     private $heartbeat;
 
-    /** @var integer session's timeout */
-    private $timeout;
+    /** @var integer[] session's and heartbeat's timeouts */
+    private $timeouts;
 
     /** @var string[] supported upgrades */
     private $upgrades;
 
-    public function __construct($id, $heartbeat, $timeout, array $upgrades)
+    public function __construct($id, $interval, $timeout, array $upgrades)
     {
         $this->id        = $id;
-        $this->timeout   = $timeout;
         $this->upgrades  = $upgrades;
-        $this->heartbeat = $heartbeat;
+        $this->heartbeat = time();
+
+        $this->timeouts  = ['timeout'  => $timeout,
+                            'interval' => $interval];
     }
 
     /** The property should not be modified, hence the private accessibility on them */
     public function __get($prop)
     {
-        static $list = ['id', 'heartbeat', 'timeout', 'upgrades'];
+        static $list = ['id', 'upgrades'];
 
         if (!in_array($prop, $list)) {
             throw new InvalidArgumentException(sprintf('Unknown property "%s" for the Session object. Only the following are availables : ["%s"]', $prop, implode('", "', $list)));
         }
 
         return $this->$prop;
+    }
+
+    /**
+     * Checks whether a new heartbeat is necessary, and does a new heartbeat if it is the case
+     *
+     * @return Boolean true if there was a heartbeat, false otherwise
+     */
+    public function needsHeartbeat()
+    {
+        if (0 < $this->timeouts['interval'] && time() > ($this->timeouts['interval'] + $this->heartbeat - 5)) {
+            $this->heartbeat = time();
+
+            return true;
+        }
+
+        return false;
     }
 }
 
