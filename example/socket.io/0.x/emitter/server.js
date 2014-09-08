e61dbb5d799d2aeb1cbcb648fe27d257700f7020
Baptiste Clavié <clavie.b@gmail.com>
2014-09-08 12:50:55 +0200

    Add some examples for Socket.io 0.x

---
+++ example/socket.io/0.x/emitter/server.js
@@ -0,0 +1,25 @@
+var logger = require('winston'),
+    port   = 1337;
+
+var io = require('socket.io').listen(port);
+
+// Logger config
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, { colorize: true, timestamp: true });
+logger.info('SocketIO > listening on port ' + port);
+
+io.sockets.on('connection', function (socket){
+    var nb = 0;
+
+    logger.info('SocketIO > Connected socket ' + socket.id);
+
+    socket.on('broadcast', function (message) {
+        ++nb;
+        logger.info('ElephantIO broadcast > ' + JSON.stringify(message));
+    });
+
+    socket.on('disconnect', function () {
+        logger.info('SocketIO : Received ' + nb + ' messages');
+        logger.info('SocketIO > Disconnected socket ' + socket.id);
+    });
+});
