3ef8c3d59568af57f5ea55394b8c315b5fa98d49
mshaverdo <m.shaverdo@gmail.com>
2015-03-17 11:57:36 +0300

    Fix missing bytes while reading extended frame length

--- src/Engine/AbstractSocketIO.php
+++ src/Engine/AbstractSocketIO.php
@@ -119,7 +119,8 @@ abstract class AbstractSocketIO implements EngineInterface
             break;
 
             case 0x7E: // 126
-                $bytes = unpack('n', fread($this->stream, 2));
+                $data .= $bytes = fread($this->stream, 2);
+                $bytes = unpack('n', $bytes);
 
                 if (empty($bytes[1])) {
                     throw new RuntimeException('Invalid extended packet len');
@@ -140,7 +141,8 @@ abstract class AbstractSocketIO implements EngineInterface
                  *
                  * {@link http://stackoverflow.com/questions/14405751/pack-and-unpack-64-bit-integer}
                  */
-                list($left, $right) = array_values(unpack('N2', fread($this->stream, 8)));
+                $data .= $bytes = fread($this->stream, 8);
+                list($left, $right) = array_values(unpack('N2', $bytes));
                 $length = $left << 32 | $right;
             break;
         }
