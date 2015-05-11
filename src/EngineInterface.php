19c6191e037c6e7045957545e79c5f06f7a113b5
Vincent Dieltiens <and1hotsauce@gmail.com>
2015-05-12 00:24:48 +0200

    Add `of` method to let engines register a namespace

--- src/EngineInterface.php
+++ src/EngineInterface.php
@@ -57,5 +57,12 @@ interface EngineInterface
 
     /** Gets the name of the engine */
     public function getName();
+
+    /** 
+     * Sets the namespace for the next messages
+     *
+     * @param string $namespace the namespace
+     */
+    public function of($namespace);
 }
 
